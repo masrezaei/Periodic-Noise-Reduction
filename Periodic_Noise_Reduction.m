4979e14d86393fb72847b86dc845dfaa8c65a84d
Masoud <Masoudrezaei86@gmail.com>
2020-10-17 19:18:31 +0330

    FFT calculation

--- Periodic_Noise_Reduction.m
+++ Periodic_Noise_Reduction.m
@@ -7,3 +7,14 @@ img = imread('Lenna.png');
 
 img = rgb2gray(img);                    %change rgb image to grayscale
 img = imresize(img,[128 128]);
+
+% Frequency Domain
+IMG = fft2(img);                
+U = ones(size(IMG));
+for i = 1:size(U,1)
+    for j = 1:size(U,2)
+        U(i,j) = (-1)^(i+j);
+    end
+end
+IMG = IMG.*U;
+
